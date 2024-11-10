ed7c79a291779cc028c9bd5472315d62a3cabcbd
Vasile <vasile.ceban1@isa.utm.md>
2024-11-10 12:00:04 +0200

    implement Flyweight Pattern to manage a large number of similar Book objects efficiently

---
+++ src/Lab_2/FlyweightPattern/FlyweightBookFactory.java
@@ -0,0 +1,20 @@
+package Lab_2.FlyweightPattern;
+
+import Lab_1.Book;
+import Lab_1.BookFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlyweightBookFactory {
+    private Map<String, Book> bookCache = new HashMap<>();
+
+    public Book getBook(String title, String author, double price, String type) {
+        String key = title + "-" + author;
+        if (!bookCache.containsKey(key)) {
+            bookCache.put(key, BookFactory.createBook(type, title, author, price));
+        }
+        return bookCache.get(key);
+    }
+}
+
